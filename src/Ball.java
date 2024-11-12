9910dc9e5ff701b51399a77e756d491b3d0cda55
chris <chrishicks.w@gmail.com>
2024-11-12 15:24:59 -0600

    Added comments

--- src/Ball.java
+++ src/Ball.java
@@ -7,16 +7,22 @@ import java.util.Random;
 class Ball extends JPanel implements ActionListener {
     Random random = new Random();
     int diameter = 50;
+
+    // Initial position for ball is randomized within frame
     int x = random.nextInt(500 - diameter);
     int y = random.nextInt(500 - diameter);
 
-    int dx = 7;
-    int dy = 15;
+    /*
+    TODO: add slider so that the speed can be adjusted in frame
+     */
+    int dx = 7; // Horizontal speed x
+    int dy = 15; // Horizontal speed y
 
+    // Timer to update the ball's position
     Timer timer;
 
     public Ball() {
-        // Timer to move the ball
+        // Initialize the timer
         timer = new Timer(10, this);
         timer.start();
     }
@@ -25,22 +31,26 @@ class Ball extends JPanel implements ActionListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        /*
+        TODO: some way to change the color of the ball in frame (maybe)
+         */
         g.setColor(Color.WHITE);
         g.fillOval (x, y, diameter, diameter);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        x += dx;
-        y += dy;
+        x += dx; // Update x
+        y += dy; // Update y
 
+        // Checks if the ball has reached the edge of the frame
         if (x < 0 || x > getWidth() - diameter) {
-            dx = -dx;
+            dx = -dx; // If so, reverses either the x or y
         }
         if (y < 0 || y > getHeight() - diameter) {
             dy = -dy;
         }
-        repaint();
+        repaint(); // Repaint the panel to update the ball's position
     }
 
 }
